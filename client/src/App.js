5522754384a38a81ebde7051c2bd4fd3f55f80ff
sravani <sandhyapyla.8@gmail.com>
2024-06-28 19:01:05 +0530

    working on home page with carosel with dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './customer/components/Navigation/Navigation';
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./customer/components/Navigation/Navigation";
+import HomePage from "./customer/pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="">
-      <Navigation/>
+      <Navigation />
       <div>
-       home
+        <HomePage />
       </div>
     </div>
   );
---
+++ client/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeSectionCard = () => {
+  return (
+    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 border border-black'>
+        <div className='h-[13rem] w-[10rem]'>
+
+         <img className='object-cover object-top w-full h-full' src='https://rukminim1.flixcart.com/image/612/612/k4d27ww0/shirt/q/w/t/l-el024-el-senor-original-imafnadnjp5pq6tg.jpeg?q=70' alt=''/>
+        </div>
+        <div className='p-4'>
+            <h3 className='text-lg font-medium text-gray-900'>El Senor</h3>
+            <p className='mt-2 text-sm text-gray-500'>Men Regular Fit Solid Cut Away Collar Formal Shirt</p>
+        </div>
+    </div>
+  )
+}
+
+export default HomeSectionCard
