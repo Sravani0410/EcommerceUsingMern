fb95484cc6d3e469e05b00c371410f22550e5114
sravani <sandhyapyla.8@gmail.com>
2024-06-29 14:34:09 +0530

    carousel is working but data is dummy

--- client/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ client/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,32 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { Button } from "@mui/material";
 
-const responsive = {
-  0: { items: 1 },
-  720: { items: 3 },
-  1024: { items: 5.5 },
-};
-const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
-const HomeSectionCarousel = () => {
+const HomeSectionCarousel = ({ data,sectionName }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5  },
+  };
+  const items = data
+    .slice(activeIndex, activeIndex + 10)
+    .map((item) => <HomeSectionCard product={item} />);
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+
+  const slideNext = () => setActiveIndex(activeIndex + 1);
+
+  const syncActiveIndex = (item) => setActiveIndex(item);
+
   return (
-    <div className="px-4 lg:px-8 border border-black">
+    <div className="border">
+      <h2 className="text-2xl font-extrabold text-gray-800 py-5">{sectionName}</h2>
       <div className="relative p-5">
         <AliceCarousel
           items={items}
-          responsive={responsive}
           disableButtonsControls
-          infinite
+          responsive={responsive}
           disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
         />
-        <Button varient="contained" className="z-50 bg-white" sx={{position:'absolute',top:"8rem", right:'0rem',transform:'translateX(50%) rotate(90deg)',bgcolor:"white"}} aria-label="next">
-            <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)',color:"black"}}/>
-        </Button>
-        <Button varient="contained" className="z-50 bg-white" sx={{position:'absolute',top:"8rem", left:'0rem',transform:'translateX(50%) rotate(-90deg)',bgcolor:"white"}} aria-label="next">
-            <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)',color:"black"}}/>
-        </Button>
+        {activeIndex !== items.length - 5 && (
+          <Button
+            variant="contained"
+            className="z-50 bg-white"
+            onClick={slideNext}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+        {activeIndex !== 0 && (
+          <Button
+            variant="contained"
+            className="z-50 "
+            onClick={slidePrev}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="prev"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
       </div>
     </div>
   );
